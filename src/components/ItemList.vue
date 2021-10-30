7e4e44eb790799b4323a0446e2fd3ff6732768c6
breno-bortot <brenobortot@gmail.com>
2021-10-30 17:05:49 -0300

    adjusting table layout of the list

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <li
+  <div class="table-active">
+    <!-- <tr>
+      <th scope="row">Active</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr> -->
+    <!-- <li
       class="list-group-item d-flex justify-content-between align-items-center"
     >
       <span>{{ product.id }}</span>
       <span>{{ product.name }}</span>
-      <span>{{ product.stock }}</span>
-      <span>{{ product.price }}</span>
-
-      <span class="badge bg-primary rounded-pill">14</span>
-    </li>
+      <span>{{ product.price }}</span>class="badge bg-primary rounded-pill">
+      <span class="badge bg-primary rounded-pill">{{ product.stock }}</span>
+    </li> -->
   </div>
 </template>
 <script>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="card-body">
-    <ul :key="product.id" v-for="product in products" class="list-group">
+    <table class="table table-hover">
+      <thead>
+        <tr class="table-dark">
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Price $</th>
+          <th class="text-end" scope="col">Stock</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="product.id" v-for="product in products" class="table-active">
+          <th scope="row">{{ product.id }}</th>
+          <td>{{ product.name }}</td>
+          <td>{{ product.price.toFixed(2) }}</td>
+          <td class="badge bg-success float-end mt-1 me-2">
+            {{ product.stock }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <ul :key="product.id" v-for="product in products" class="list-group">
       <ItemList :product="product" />
-    </ul>
+    </ul> -->
   </div>
 </template>
 <script>
